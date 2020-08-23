284e227d94be844ecaaf2a5239a9023ddf32e9e8
wukun <1803641586@qq.com>
2020-08-23 23:20:18 +0800

    上传测试代码

--- miniprogram/pages/index/update.js
+++ miniprogram/pages/index/update.js
@@ -41,7 +41,6 @@ Page({
     })
   },
   Ondelete(event){
-     console.log(event.detail.file.name)
      wx.cloud.deleteFile({
         fileList:[event.detail.file.name],
         success(res){
@@ -105,7 +104,7 @@ Page({
   
   //将数据添加到数据库
   add:function(){
-      let this2 = this;
+     
       //上传动态的时间
       this.data.timestamp = Date.parse(new Date());
       this.data.timestamp =this.data.timestamp / 1000;
@@ -126,11 +125,11 @@ Page({
       this.data.timestamp=Y+"-"+M+"-"+D+" "+h+":"+m
     cn.add({
       data:{
-        concent:this2.data.value,
-        fileList:this2.data.fileList,
-        date: this2.data.timestamp,
-        time:this2.data.date,
-        userInfo:this2.data.userInfo
+        concent:this.data.value,
+        fileList:this.data.fileList,
+        date: this.data.timestamp,
+        time:this.data.date,
+        userInfo:this.data.userInfo
       }
     }).then(res => {
       console.log("zheli",res)
@@ -141,14 +140,12 @@ Page({
     })
     //清空页面
     const { fileList = [] } = [];
-    this2.setData({ fileList });
-    this2.setData({value:""})
+    this.setData({ fileList });
+    this.setData({value:""})
     wx.switchTab({
       url: '../index/index',
     })
-       console.log("结束发布",this2.data.userInfo)
        }
-
     })
   
    
