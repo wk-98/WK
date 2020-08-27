1b43fa9ac85740e420ed4fab8c03df60e038d26b
wukun <1803641586@qq.com>
2020-08-28 00:14:31 +0800

    实现关注、取关与删除自己动态功能
    1、signal变量用来判断页面渲染是否已关注
    2、使用了全局变量

--- miniprogram/pages/todoinfo/todoinfo.js
+++ miniprogram/pages/todoinfo/todoinfo.js
@@ -9,7 +9,12 @@ Page({
     task:null,
     com:null,
     avatarUrl1:null,
-    userInfo1:null
+    userInfo1:null,
+    icon_color:'#000',
+    icon:'like-o',
+    signal:1,
+    _id:'',
+   
 
   },
   pagedata:{
@@ -37,21 +42,37 @@ Page({
   onLoad: function (options) {
      console.log(options.id)
       // console.log(options.id)
+      const app = getApp()
+     
       db.collection('cn').doc(options.id).get().then(res => {
         
+        if(res.data._openid==app.globalData.openid){
+          this.setData({
+            signal:2
+          })
+          
+        }else{
+          this.chexk()
+        }
         this.setData({
           task:res.data
         })
+        console.log("task",this.data.task) 
+       
+        
       }),
       db.collection('comment').where({
         _id2: options.id
       })
       .get().then(res => {
-        
+        console.log("zhel",res)
         this.setData({
           com:res.data
         })
+        console.log("com",this.data.com)
+        
       }),
+     
       wx.getSetting({
         success: res => {
           if (res.authSetting['scope.userInfo']) {
@@ -68,7 +89,211 @@ Page({
         }
       })
   },
+  //检查是否已经关注了该用户
+  chexk:function(){
+    const app = getApp() 
+   console.log("chexk方法")
+    const db = wx.cloud.database()
+    // where 查询操作
+    db.collection('concern').where({
+      // 查询条件
+      _openid:app.globalData._openid
+    })
+    .get()
+    .then(res => {
+      // 查询数据成功
+      console.log("zhixing1",res)
+      if(res.data.length!=0){
+        for(var i =0 ;i < res.data[0].B_openid.length; i++){
+        if(res.data[0].B_openid[i]._openid==this.data.task._openid){
+          this.setData({
+            icon:'like',
+            icon_color:'#f00',
+            signal:3
+          }) ;
+          break;
+        }
+      }
+      }
+      
+    }).catch(err => {
+      // 查询数据失败
+      console.log("失败",err)
+    })
+  },
 
+/**
+ * 点击关注事件,点击取消关注事件
+ */
+concern:function(event){
+ const db = wx.cloud.database()
+ const _ = db.command
+  const app = getApp() 
+  if(app.globalData.openid){
+   console.log("openid",app.globalData.openid)
+// where 查询操作
+db.collection('concern').where({
+  _openid:app.globalData._openid
+})
+.get()
+.then(res => {
+  // 查询数据成功
+  console.log("查询",res)
+ 
+  this.setData({
+    icon:'like',
+    icon_color:'#f00',
+    signal:3
+  }) 
+  if(res.data.length){
+    //更新数据
+    // update 更新操作
+    // primary key 要更新的那条数据的主键id
+    console.log("res.data._id",res.data[0]._id)
+    db.collection('concern').doc(res.data[0]._id)
+    .update({
+      // 想要更新后的数据
+      data: {
+        B_openid:_.push([{
+
+            _openid:this.data.task._openid,
+          userInfo:this.data.task.userInfo
+        }])
+      }
+    }).then(res => {
+      // 更新数据成功
+      console.log(res)
+    }).catch(err => {
+      // 更新数据失败
+      console.log(err)
+    })
+
+  }else{
+    //插入数据
+    // add 插入操作
+    db.collection('concern').add({
+      // 要插入的数据
+      data: {
+       B_openid:[{
+         _openid:this.data.task._openid,
+        userInfo:this.data.task.userInfo
+      }]
+      }
+    }).then(res => {
+      // 插入数据成功
+      console.log("插入成功",res)
+    }).catch(err => {
+      // 插入数据失败
+      console.log("插入失败",err)
+    })
+  }
+}).catch(err => {
+  // 查询数据失败
+  console.log(err)
+  
+})
+  }else{
+    wx.showToast({
+      title: '登录才能进行该操作，步骤：点击“我的”页面',
+      icon:'none'
+    })
+  }
+   
+},
+unconcern:function(event){
+  const app = getApp() 
+  if(app.globalData.openid){
+  console.log("取消关注",event)
+  this.setData({
+    icon:'like-o',
+    icon_color:'#000',
+    signal:1
+  })
+  const db = wx.cloud.database()
+  const _ = db.command
+  const app = getApp()
+  
+        // where 查询操作
+        db.collection('concern').where({
+          // 查询条件
+          _openid:app.globalData._openid
+        })
+        .get()
+        .then(res => {
+          // 查询数据成功
+          console.log(res)
+          db.collection('concern').doc(res.data[0]._id)
+          .update({
+            // 想要更新后的数据
+            data: {
+             
+              B_openid: _.pull({
+                _openid:_.eq(this.data.task._openid)
+            })
+            }
+          }).then(res => {
+            // 更新数据成功
+            console.log(res)
+          }).catch(err => {
+            // 更新数据失败
+            console.log(err)
+          })
+        }).catch(err => {
+          // 查询数据失败
+          console.log(err)
+        })
+      }else{
+        wx.showToast({
+          title: '登录才能进行该操作，步骤：点击“我的”页面->“点击登录”',
+          icon:'none'
+        })
+      }
+  
+},
+//删除自己的动态
+delete(){
+  const db = wx.cloud.database()
+  let this1 = this;
+  const app = getApp()
+  //console.log("app",app.flag)
+  wx.showModal({
+    cancelColor: 'cancelColor',
+    title: '提示',
+    content: '确定要删除该动态吗？',
+
+    success (res) {
+      if (res.confirm) {
+        console.log('用户点击确定')
+        //const _ = db.command
+    // remove 删除操作
+    // primary key 要删除的那条数据的主键id
+    db.collection('cn').doc(this1.data.task._id)
+    .remove()
+    .then(res => {
+      // 删除数据成功
+      console.log("成功删除",res)
+      app.flag = 1,
+      wx.navigateBack({
+        delta: 0,
+         success:res=>{
+           wx.showToast({
+           title: '删除成功',
+           icon:'success'
+          })
+        }
+      })
+     
+    }).catch(err => {
+      // 删除数据失败
+      console.log(err)
+    })
+      } else if (res.cancel) {
+        console.log('用户点击取消')
+      }
+    }
+  })
+  
+},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -77,47 +302,7 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
+  
 
   onCounterInc: function() {
     const newCount = this.data.task.likes + 1
